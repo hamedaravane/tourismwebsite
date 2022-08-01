c9473dec669ab16f47695e10959f9da25d877301
hamedaravane <hamedaravane@gmail.com>
2022-08-01 17:51:44 +0430

    creating footer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/pages/Home";
+import Footer from "./components/footer/Footer";
 
 function App() {
     return (
-        <Router>
-            <Navbar/>
-            <Routes>
-                <Route path='/' exact component={Home}/>
-            </Routes>
-            <Home/>
-        </Router>
+        <>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path='/' exact component={Home}/>
+                </Routes>
+                <Home/>
+            </Router>
+            <Footer/>
+        </>
     );
 }
 
